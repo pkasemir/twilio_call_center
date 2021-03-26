fdbfd827ccaf87f011412a690e0c0cf809a3de74
Paul Kasemir <paul.kasemir@ui.com>
2021-03-25 20:56:15 -0600

    Add url namespace for this app

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'twilio_call_center'
 urlpatterns = [
     path('call-menu', views.call_menu, name='call-menu'),
     path('call-action', views.call_action, name='call-action'),
