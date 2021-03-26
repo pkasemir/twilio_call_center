80c758e347b9715cddfc8261bbba55cfc9f10ab6
Paul Kasemir <paul.kasemir@ui.com>
2021-03-26 00:39:39 -0600

    feature: allow various menus and submenus fully dynamic.

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'twilio_call_center'
 urlpatterns = [
-    path('call-menu', views.call_menu, name='call-menu'),
-    path('call-action', views.call_action, name='call-action'),
-    path('call-end', views.call_end, name='call-end'),
+    path('<slug:name>/call-menu', views.call_menu, name='call-menu'),
+    path('<slug:name>/call-action', views.call_action, name='call-action'),
+    path('<slug:name>/call-end', views.call_end, name='call-end'),
 ]
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.urls import reverse
 
 try:
@@ -19,10 +19,15 @@ except Exception as e:
 
 from twilio.twiml.voice_response import VoiceResponse
 
-from .models import MenuItem, twilio_default_transfer
+from .models import Menu, MenuItem, twilio_default_transfer, \
+    twilio_default_voice
 
 
-def twilio_say(response, message, voice='woman', **kwargs):
+def twilio_say(menu, response, message, **kwargs):
+    if menu.voice is None:
+        voice = twilio_default_voice
+    else:
+        voice = menu.voice.voice
     response.say(message, voice=voice, **kwargs)
 
 
@@ -34,82 +39,110 @@ def get_query_dict(request):
     return {}
 
 
+def call_reverse(name, page):
+    return reverse("twilio_call_center:" + page, kwargs={"name":name})
+
+
+def get_menu(name):
+    menu = Menu.objects.filter(enabled=True, name=name)
+    if len(menu) == 0:
+        raise Http404("Call Center menu {} doesn't exist.".format(name))
+
+    return menu.first()
+
+
+def get_menu_items(menu):
+    items = MenuItem.objects.filter(enabled=True, menu=menu) \
+        .order_by('menu_digit')
+    if len(items) == 0:
+        raise Http404("Call Center menu {} has no items.".format(menu.name))
+
+    return items
+
+
 @twilio_view
-def call_menu(request):
+def call_menu(request, name):
     response = VoiceResponse()
+    menu = get_menu(name)
+    items = get_menu_items(menu)
 
     # start twilio menu response
     with response.gather(
-        num_digits=1, action=reverse("twilio_call_center:call-action"), method="POST",
+        num_digits=1, action=call_reverse(name, "call-action"), method="POST",
         timeout=10
     ) as g:
         last_digit = -1
-        menu_text = "Thank you for calling White Horse Health and Wellness!"
-        entries = MenuItem.objects.filter(enabled=True).order_by('menu_digit')
-        for entry in entries:
-            # skip empty entries
-            if not entry.menu_text and not entry.action_text \
-                    and not entry.action_phone:
+        menu_text = ""
+        if menu.greeting_text is not None:
+            menu_text += menu.greeting_text
+        for item in items:
+            # skip empty items
+            if not item.menu_text and not item.action_text \
+                    and not item.action_phone:
                 continue
-            if last_digit >= entry.menu_digit:
+            if last_digit >= item.menu_digit:
                 continue
 
-            last_digit = entry.menu_digit
+            last_digit = item.menu_digit
 
-            if entry.menu_text:
-                menu_text += " Press {} {}.".format(entry.menu_digit,
-                                                    entry.menu_text)
+            if item.menu_text:
+                menu_text += " Press {} {}.".format(item.menu_digit,
+                                                    item.menu_text)
 
-        twilio_say(g, menu_text)
+        twilio_say(menu, g, menu_text)
     return response
 
 
 @twilio_view
-def call_action(request):
+def call_action(request, name):
     response = VoiceResponse()
-    last_digit = -1
+    menu = get_menu(name)
+    items = get_menu_items(menu)
     action_text = None
     action_phone = None
+    action_url = None
+    next_menu = name
+    next_page = None
 
     query_dict = get_query_dict(request)
     digit = query_dict['Digits']
 
-    entries = MenuItem.objects.filter(enabled=True, menu_digit=digit) \
-        .order_by('menu_digit')
-    for entry in entries:
-        # skip empty entries
-        if not entry.menu_text and not entry.action_text \
-                and not entry.action_phone:
-            continue
-        if last_digit >= entry.menu_digit:
-            continue
+    digit_items = items.filter(menu_digit=digit)
+    if len(digit_items):
+        item = digit_items.first()
 
-        last_digit = entry.menu_digit
-
-        if digit == str(entry.menu_digit):
-            if entry.action_text:
-                action_text = entry.action_text
-            if entry.action_phone:
-                action_phone = entry.action_phone
-                if action_text is None:
-                    action_text = twilio_default_transfer
-            break
+        if item.action_text:
+            action_text = item.action_text
+        if item.action_phone:
+            action_phone = item.action_phone
+            if action_text is None:
+                action_text = twilio_default_transfer
+        if item.action_url:
+            action_url = item.action_url
+        elif item.action_submenu:
+            next_menu = item.action_submenu.name
+            next_page = "call-menu"
 
     if action_text is not None:
-        twilio_say(response, action_text)
+        twilio_say(menu, response, action_text)
     if action_phone is None:
+        next_page = "call-menu"
         if action_text is not None:
             response.pause(1)
-        response.redirect(reverse("twilio_call_center:call-menu"))
     else:
+        if next_page is None:
+            next_page = "call-end"
         response.dial(action_phone)
-        response.redirect(reverse("twilio_call_center:call-end"))
+    if action_url is None:
+        action_url = call_reverse(next_menu, next_page)
+    response.redirect(action_url)
     return response
 
 
 @twilio_view
-def call_end(request):
+def call_end(request, name):
     response = VoiceResponse()
-    twilio_say(response, 'Goodbye.')
+    menu = get_menu(name)
+    twilio_say(menu, response, 'Goodbye.')
     response.hangup()
     return response
