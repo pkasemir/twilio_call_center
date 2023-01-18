2d1303847ed0d8c2fa71fcabfd28152e4f8821f7
Paul Kasemir <paul.kasemir@gmail.com>
2023-01-17 21:51:40 -0700

    Allow html files to be packaged also

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
         "Framework :: Django",
     ],
     package_dir={"": "src"},
+    package_data={"twilio_call_center": ["templates/*/*.html"]},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
