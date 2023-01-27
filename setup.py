b40380f2de74fb64ef27d45e8ec9b1342484af49
Paul Kasemir <paul.kasemir@gmail.com>
2023-01-26 19:55:58 -0700

    chore: set version 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="twilio_call_center",
-    version="1.2.0",
+    version="1.3.0",
     author="Paul Kasemir",
     author_email="paul.kasemir@gmail.com",
     description="A django app for dynamic twilio call center",
